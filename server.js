87f483ee23b05b0c5e6a7372bcd9023c3283f774
nisargkolhe <nisargkolhe@gmail.com>
2017-09-16 15:13:18 -0400

    Added a function I forgot to put in

--- server.js
+++ server.js
@@ -37,7 +37,26 @@ app.get('/webhook/', function (req, res) {
 	    res.sendStatus(200)
 	}
 	res.send('Error, wrong token')
-})
+});
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
 
 var server = app.listen(app.get('port'), function () {
   console.log('Listening at http://localhost:' + app.get('port'));
