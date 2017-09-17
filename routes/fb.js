8f708a9751e871068d6bb1c9b2095d2e8bc46243
nisargkolhe <nisargkolhe@gmail.com>
2017-09-17 00:22:37 -0400

    added if

--- routes/fb.js
+++ routes/fb.js
@@ -12,52 +12,54 @@ exports.webhook = function(req, res) {
 		res.send('Error, wrong token')
 	} else if(req.method == 'POST'){
 		let messaging_events = req.body.entry[0].messaging
-		for (let i = 0; i < messaging_events.length; i++) {
-			let event = req.body.entry[0].messaging[i]
-			let sender = event.sender.id
+		if(messaging_events){
+			for (let i = 0; i < messaging_events.length; i++) {
+				let event = req.body.entry[0].messaging[i]
+				let sender = event.sender.id
 
-			if (event.message && event.message.text) {
-				request({
-			    url: 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/16b92656-8832-4d56-92ea-15f72fe69b3b',
-			    qs: {
-			      'subscription-key': '6c099f46b90c4ab8b85b31fde3e7a756',
-			      'verbose': 'true',
-			      'q': event.message.text
-			    },
-			    method: 'GET'
-			  }, function(error, response, body) {
-			    body = JSON.parse(body)
-			    console.log('ms body', body);
-					var intent = body.topScoringIntent.intent;
-					if (intent === 'Shop'){
-						var product = body.entities[0].entity;
-						var obj = {
-							category : "sss",
-						    maxAsk : 500,
-						    minAsk : 100,
-						    city : "Seattle",
-						    query : product
+				if (event.message && event.message.text) {
+					request({
+				    url: 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/16b92656-8832-4d56-92ea-15f72fe69b3b',
+				    qs: {
+				      'subscription-key': '6c099f46b90c4ab8b85b31fde3e7a756',
+				      'verbose': 'true',
+				      'q': event.message.text
+				    },
+				    method: 'GET'
+				  }, function(error, response, body) {
+				    body = JSON.parse(body)
+				    console.log('ms body', body);
+						var intent = body.topScoringIntent.intent;
+						if (intent === 'Shop'){
+							var product = body.entities[0].entity;
+							var obj = {
+								category : "sss",
+							    maxAsk : 500,
+							    minAsk : 100,
+							    city : "Seattle",
+							    query : product
+							}
+							sendTextMessage(sender, "Okay! Give me a sec while I look for a " + product);
+							craigslist.getListings(obj, function(res){
+								console.log('res after getListings', res);
+								sendListingCardsMessage(sender, res);
+							});
+						} else {
+							sendTextMessage(sender, "Text received, echo: " + event.message.text.substring(0, 200));
 						}
-						sendTextMessage(sender, "Okay! Give me a sec while I look for a " + product);
-						craigslist.getListings(obj, function(res){
-							console.log('res after getListings', res);
-							sendListingCardsMessage(sender, res);
-						});
-					} else {
-						sendTextMessage(sender, "Text received, echo: " + event.message.text.substring(0, 200));
-					}
-			  });
-			}
-			if (event.postback) {
-				//event.postback = JSON.parse(event.postback);
-				console.log('event.postback', event.postback);
-				if(event.postback.type == "buy"){ 
-		          sendTextMessage(sender, "Okay! Here's the reply URL - " + event.postback.replyUrl + ". You need to click it to get the seller's number."); 
-		          sendTextMessage(sender, "If you'd like I can negotiate the price on your behalf. Would you like me to do that?"); 
-		        } 
-				// let text = JSON.stringify(event.postback)
-				// sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-				// continue
+				  });
+				}
+				if (event.postback) {
+					//event.postback = JSON.parse(event.postback);
+					console.log('event.postback', event.postback);
+					if(event.postback.type == "buy"){ 
+			          sendTextMessage(sender, "Okay! Here's the reply URL - " + event.postback.replyUrl + ". You need to click it to get the seller's number."); 
+			          sendTextMessage(sender, "If you'd like I can negotiate the price on your behalf. Would you like me to do that?"); 
+			        } 
+					// let text = JSON.stringify(event.postback)
+					// sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+					// continue
+				}
 			}
 		}
 		res.sendStatus(200)
